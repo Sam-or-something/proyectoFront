da4caa29b141eb336e7ccb7e5ea089eacac12dee
Sofía <101577333+SofiaCirelli@users.noreply.github.com>
2023-09-20 10:18:59 -0300

    arreglo

--- pages/cursos.tsx
+++ pages/cursos.tsx
@@ -2,12 +2,12 @@
 import CartaCurso from '@/components/cartaCurso';
 import React, { useState, useEffect } from 'react';
 
-/*export default function cursos() {
+export default function cursos() {
     
     return(
         <main className={`flex min-h-screen flex-col items-center justify-between p-24`}>
             <div className="bg-white shadow-md rounded px-10 pt-8 pb-10 mb-4 w-xl max-w-xl">
-                <button type="button" id="boton" onClick={handler} className="object-right bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" ></button>   
+                <button type="button" id="boton" className="object-right bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" ></button>   
             </div>
 
             <div>
@@ -17,7 +17,7 @@ import React, { useState, useEffect } from 'react';
     )
 }
 
-async function handler() {
+/*async function handler() {
 
     const data = {
         token : localStorage.getItem('jwtToken') as string,
