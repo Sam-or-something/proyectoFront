05ed7ba55ad47ef110f87803de4adef45c41acf6
Sofía <101577333+SofiaCirelli@users.noreply.github.com>
2023-11-29 18:47:39 -0300

    :) p.4

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/router";
 function CerrarSesion(){
     document.cookie = `authToken=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;`
     sessionStorage.removeItem(authToken)
-    useRouter().push("/")
+    useRouter().push("/login")
 }
 
 function Navbar() {
